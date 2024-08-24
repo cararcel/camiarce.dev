12f74a47015457c6c5335448c02584c60f3f395a
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-08-24 15:19:06 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'black-small': '2px 3px 0 0 rgba(0, 0, 0, 1)',
+        'black-small-raised': '6px 6px 0 0 rgba(0, 0, 0, 1)',
+        black: '4px 6px 0 0 rgba(0, 0, 0, 1)',
+        'black-raised': '6px 6px 0 0 rgba(0, 0, 0, 1)'
+      },
+      fontFamily: {
+        handwritten: 'indie_flower'
+      },
+      keyframes: {
+        scale: {
+          '0%, 100%': { transform: 'scale(1);' },
+          '50%': { transform: 'scale(1.1);' },
+        }
+      },
+      animation: {
+        scale: 'scale 1s ease-in-out infinite',
+      }
+    },
+  },
+  plugins: [],
+}
